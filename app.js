22976f3d7540759cb54a5cb1bca45b668c0d3edc
Brian Young <pastpostcard@gmail.com>
2022-01-10 11:12:13 -0800

    Updated to use Class to create each decodeaquote

--- app.js
+++ app.js
@@ -2,60 +2,63 @@
 let testQuote = "Happiness often sneaks in through a door you didn't know you left open."
 let testQuote2 = "The quick brown fox jumps over the lazy dog"
 
+//redo this as a class
 
 
-function randomizeQuote(quote) {
-    //lowercase quote
-    let lcQuote = quote.toLowerCase()
-    let letters = {}
-    let decode = ''
 
-    //iterate through lcQuote
-    for (let i = 0; i < lcQuote.length; i++) {
 
-        //if not letter, add to decodestring
-        if (lcQuote.charCodeAt(i) >= 97 && lcQuote.charCodeAt(i) <= 122) {
-            // in object then add to decode string
-            if (letters[lcQuote[i]]) {
-                decode += letters[lcQuote[i]]
-            } else {
-                //if letter not in letters object 
-                //create new random letter and add to string
-                decode += getLetter(lcQuote[i])
-            }
-        } else {
-            //add non letters to the string
-            decode += lcQuote[i]
-        }
+class DecodeQuote {
+    constructor(quote) {
+        this.lcQuote = quote.toLowerCase()
+        this.letters = {}
+        this.decodeThis = this.randomizeQuote()
+        this.userInput = ''
     }
 
-    function getLetter(currentLetter) {
+    randomizeQuote() {
 
-        //create random number
+        let codedQuote = ''
+        for (let i = 0; i < this.lcQuote.length; i++) {
+            //if not letter, add to decode string
+            if (this.lcQuote.charCodeAt(i) >= 97 && this.lcQuote.charCodeAt(i) <= 122) {
+                // in object then add to decode string
+                if (this.letters[this.lcQuote[i]]) {
+                    codedQuote += this.letters[this.lcQuote[i]]
+                } else {
+                    //if letter not in letters object 
+                    //create new random letter and add to string
+                    codedQuote += this.getLetter(this.lcQuote[i])
+                }
+            } else {
+                //add non letters to the string
+                codedQuote += this.lcQuote[i]
+            }
+        }
+        return codedQuote
+    }
+
+    getLetter(currentLetter) {
+
         let newCharCode = Math.floor(Math.random() * (123 - 97) + 97)
         //verify random number does not exist in object
-        if (Object.values(letters).includes(String.fromCharCode(newCharCode))) {
+        if (Object.values(this.letters).includes(String.fromCharCode(newCharCode))) {
             //if it does get another random number
-            return getLetter(currentLetter)
+            return this.getLetter(currentLetter)
         }
         //add current letter to object
-        letters[currentLetter] = String.fromCharCode(newCharCode)
+        this.letters[currentLetter] = String.fromCharCode(newCharCode)
         //return new letter 
         return String.fromCharCode(newCharCode)
     }
-    console.log(letters)
-    console.log(quote)
-    return decode
+
+    // checkQuote(quote, userInput) {
+    //     if (randomizeQuote(quote) === userInput) return 'Correctly Decode'
+    // }
+
 }
 
 
 
-
-
-
-function checkQuote(quote, userInput) {
-    if (randomizeQuote(quote) === userInput) return 'Correctly Decode'
-}
 
 //freeQuote API
 //https://github.com/lukePeavey/quotable#get-random-quote
