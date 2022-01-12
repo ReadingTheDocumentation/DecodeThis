a5834a86512b45268c262d48f11cc924b3a40744
Brian Young <pastpostcard@gmail.com>
2022-01-12 11:31:28 -0800

    I should have commited more and will in the future, but this now works! A lot of cleaning up to do, but first self directed project from scratch.

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 Writing a decodaquote website from scratch using JS and Bulma.
 
 Version 1 Goals
+[] Get daily quote from an API
 [X]Randomize a quote
     [X]just letters leave punctuation as is
+[]Create a test suite/assertion tests
+[]
 []Create an easy way to swap in letters
 []Create some kind of undo state
 [X]Check if decode a quote is correct
 []create congratulations alert if it's correct
+[]Style in Bulma
+[]Host on GitHub
+
+
+
+//mockup https://excalidraw.com/#room=27239d9c23fcd70fc14f,YvJQP8y6frEkB6BZSxr73w
+
+//Keyboard from https://gist.github.com/itszero/972142
--- app.js
+++ app.js
@@ -1,14 +1,135 @@
 
-let testQuote = "Happiness often sneaks in through a door you didn't know you left open."
-let testQuote2 = "The quick brown fox jumps over the lazy dog"
 
-//redo this as a class
 
+let runtime;
+let quote = document.querySelector('.quote')
+
+document.addEventListener("DOMContentLoaded", function () {
+    getQuote()
+});
+// ***** Modal ****//
+
+let modal = document.querySelector('.modal')
+
+let closeModal = document.querySelector('.delete')
+closeModal.addEventListener('click', (e) => {
+    modal.classList.remove('is-active')
+})
+
+
+// ***** Move Cursor ****//
+let footer = document.querySelector('.card-footer')
+let fromBox = document.querySelector('#from')
+let toBox = document.querySelector('#to')
+let button = document.querySelector('#swap')
+footer.addEventListener('keyup', (e) => {
+    if (fromBox.value.length >= 1) {
+        toBox.focus()
+    }
+    if (toBox.value.length >= 1) {
+        button.focus()
+    }
+    if (e.keyCode === 8) {
+        toBox.value = ''
+        fromBox.value = ''
+        fromBox.focus()
+    }
+})
+
+
+// ***** Player Inputs ****//
+let app = document.querySelector('section')
+app.addEventListener("click", (e) => {
+    e.preventDefault()
+    let from = ''
+    let to = ''
+    let del = false
+    let removeTag = e.target.parentNode;
+
+
+    if (e.target.id === "swap" || e.target.id === "tag") {
+        if (e.target.id === "tag") { del = true }
+        let par = e.target.parentNode.children;
+        for (let i = 0; i < par.length; i++) {
+
+            if (par[i].id === 'from') {
+                from = par[i].innerText || par[i].value
+                par[i].value = ''
+
+            }
+            if (par[i].id === 'to') {
+                to = par[i].innerText || par[i].value
+                par[i].value = ''
+            }
+        }
+
+        if (del === false) {
+            createTag(from, to)
+            runtime.changeQuote(quote, from, to)
+        }
+        if (del === true) {
+            runtime.changeQuote(quote, to, from, del)
+            deleteTag(removeTag)
+        }
+        runtime.fullyDecoded()
+    }
+    fromBox.focus()
+
+
+
+})
+
+
+// ***** Tag Function ****//
+
+function createTag(from, to) {
+    if (from && to) {
+        const changes = document.querySelector('.changes')
+        changes.innerHTML +=
+            `<div class="tageach">
+        <div class="tags has-addons">
+                <span class="tag is-link" id="to">${to}</span>
+                <span class="tag is-primary" id="from">${from}</span>
+                <a class="button tag is-delete" id="tag"></a>
+            </div>
+            </div>`
+
+    }
+
+}
+
+function deleteTag(toDelete) {
+    toDelete.remove()
+}
+
+
+
+// ***** API Call ****//
+
+const getQuote = async () => {
+    console.log("this works")
+    try {
+
+        let res = await axios.get('https://api.quotable.io/random?maxLength=100')
+        console.log(res.data.author)
+        let dailyQuote = res.data.content
+        console.log(res.data.content)
+        let author = res.data.author
+        runtime = new DecodeQuote(dailyQuote, author)
+        runtime.spanify(runtime.decodeThis)
+    } catch (e) {
+        return
+    }
+}
+
+
+// ***** Quote Functions ****//
 
 
 
 class DecodeQuote {
-    constructor(quote) {
+    constructor(quote, author) {
+        this.author = author
         this.lcQuote = quote.toLowerCase()
         this.letters = {}
         this.decodeThis = this.randomizeQuote()
@@ -51,14 +172,67 @@ class DecodeQuote {
         return String.fromCharCode(newCharCode)
     }
 
-    // checkQuote(quote, userInput) {
-    //     if (randomizeQuote(quote) === userInput) return 'Correctly Decode'
-    // }
+    spanify(words) {
+        quote.innerHTML += `<span class="newSize">"</span>`
+        for (let i = 0; i < words.length; i++) {
+            const newSpan = document.createElement("span")
+            newSpan.innerText = words[i]
+            newSpan.id = words[i]
+            if (words.charCodeAt([i]) < 97) {
+                newSpan.className = "newSize"
+            } newSpan.className = "defaultSize"
+            quote.appendChild(newSpan)
+        }
+        quote.innerHTML += `<span class="newSize">"</span>`
+
+    }
+
+    changeQuote(el, from, to, revert) {
+        console.log(revert)
+        let element = el.children
+
+        if (!revert) {
+            for (let i = 0; i < element.length; i++) {
+                if (element[i].innerText === from && element[i].id === from) {
+                    element[i].innerText = to
+                    element[i].className = "newSize"
+                }
+
+            }
+        }
+        if (revert) {
+            for (let i = 0; i < element.length; i++) {
+                if (element[i].innerText === from && element[i].id != from) {
+                    element[i].innerText = element[i].id
+                    element[i].className = "defaultSize"
+
+                }
+            }
+        }
+
+    }
+
+    fullyDecoded() {
+        let userSolution = ''
+        let spans = quote.children
+        for (let i = 1; i < quote.children.length - 1; i++) {
+            userSolution += quote.children[i].innerText
+        }
+        if (userSolution === this.lcQuote) {
+            console.log(quote)
+            modal.querySelector('blockquote').innerText = `"${testQuote}"`
+            modal.querySelector('.author').innerText = `—${author}`
+
+            modal.classList.add('is-active')
+
+        }
+
+
+    }
+
+
 
 }
-
-
-
 
 //freeQuote API
 //https://github.com/lukePeavey/quotable#get-random-quote
