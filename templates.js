fa42702b7dfa44271473a18a013efca2eca57672
Brian Young <pastpostcard@gmail.com>
2022-01-13 21:29:55 -0800

    Major edits mostly to clean up code and increase readability

--- templates.js
+++ templates.js
@@ -3,13 +3,14 @@
 // ***** Tag Template ****//
 
 function createTag(from, to) {
+    console.log("this is called")
     if (from && to) {
         const changes = document.querySelector('.changes')
         changes.innerHTML +=
             `<div class="tageach">
         <div class="tags has-addons">
-                <span class="tag is-link" id="to">${to}</span>
-                <span class="tag is-primary" id="from">${from}</span>
+                <span class="tag is-primary" id="to">${to}</span>
+                <span class="tag is-link" id="from">${from}</span>
                 <a class="button tag is-delete" id="tag"></a>
             </div>
             </div>`
@@ -19,6 +20,30 @@ function createTag(from, to) {
 }
 
 function createSpan(character) {
+
+
+
+}
+
+
+
+function createModal(quote, author) {
+    // <div class="modal">
+    //     <div class="modal-background"></div>
+    //     <div class="modal-card">
+    //         <header class="modal-card-head">
+    //             <p class="modal-card-title">Congratulations! You Decoded It!</p>
+    //             <button class="delete" aria-label="close"></button>
+    //         </header>
+    //         <div class="modal-card-body">
+    //             <blockquote></blockquote>
+    //             <p class="author"></p>
+    //         </div>
+    //         <footer class="modal-card-foot">
+    //         </footer>
+    //     </div>
+    // </div>
+
 
 }
 
